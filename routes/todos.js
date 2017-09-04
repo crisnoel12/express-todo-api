ae05e3b99dd04756ce2549c6380dbad28f7e8a1b
Cris Cancino <crisnoel12@gmail.com>
2017-09-03 17:00:32 -0700

    refactored routes with `id` param, except update route

--- routes/todos.js
+++ routes/todos.js
@@ -24,13 +24,18 @@ router.post('/', function(req, res, next) {
     });
 });
 
-router.get('/:id', function(req, res, next) {
-    var id = mongoose.Types.ObjectId(req.params.id);
-    Todo.findById(id, function (err, todo) {
+router.param("id", function (req, res, next, id) {
+    id = mongoose.Types.ObjectId(req.params.id);
+    Todo.findById(id).exec(function (err, todo) {
         if (err) return next(err);
-        if (!todo) { err = new Error("Todo Not Found"); err.status = 404; return next(err); }
-        res.json(todo);
+        if (!todo) { err = new Error("Not Found"); err.status = 404; return next(err); }
+        req.todo = todo
+        return next();
     });
+});
+
+router.get('/:id', function(req, res, next) {
+    res.json(req.todo);
 });
 
 router.put('/:id', function (req, res, next) {
@@ -42,13 +47,9 @@ router.put('/:id', function (req, res, next) {
 });
 
 router.delete('/:id', function (req, res, next) {
-    Todo.findById(req.params.id, function (err, todo) {
+    req.todo.remove(function(err){
         if (err) return next(err);
-        if (!todo) { err = new Error("Todo Not Found"); err.status = 404; return next(err); }
-        todo.remove(function(err){
-            if (err) return next(err);
-            res.json({message: "Todo deleted!"});
-        })
+        res.json({ message: "Todo deleted!" });
     });
 });
 
